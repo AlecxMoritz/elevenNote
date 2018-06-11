b34120048af93c3bc4b3032bb392ed1b25b1a146
Alecx Moritz <hanelore@live.com>
2018-06-11 12:44:57 -0400

    action links in mod eight

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -53,5 +53,14 @@ namespace ElevenNote.WebMVC.Controllers
             var service = new NoteService(userId);
             return service;
         }
+
+        public ActionResult Details(int id)
+        {
+            var svc = CreateNoteService();
+            var model = svc.GetNoteById(id);
+
+            return View(model);
+        }
+
     }
 }
