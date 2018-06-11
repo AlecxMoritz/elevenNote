96a0d50f66fed3045c38ad0ab50d0149240ac467
Alecx Moritz <hanelore@live.com>
2018-06-11 14:54:42 -0400

    module star.2

--- ElevenNote.WebMVC/Global.asax.cs
+++ ElevenNote.WebMVC/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ElevenNote.WebMVC.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,6 +16,7 @@ namespace ElevenNote.WebMVC
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            WebApiConfig.Register();
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
